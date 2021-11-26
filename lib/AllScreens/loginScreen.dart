4a818805706d463875dc26c4759a0a7bda38f24b
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2021-11-26 14:55:48 +0000

    login Screen Added Successfully

--- lib/AllScreens/loginScreen.dart
+++ lib/AllScreens/loginScreen.dart
@@ -7,22 +7,92 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(height: 65.0,),
-          Image(image: AssetImage("images/logo.png"),
-            width: 35.0,
-            height: 350.0,
-            alignment: Alignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 65.0,),
+              Image(image: AssetImage("images/logo.png"),
+                width: 35.0,
+                height: 350.0,
+                alignment: Alignment.center,
+              ),
+
+              SizedBox(height: 15.0,),
+
+              Text(
+                "Login as a RIder",
+                style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                textAlign: TextAlign.center,
+              ),
+
+              Padding(padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 1.0,),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10.0,
+                      ),
+                    ),
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+
+                  SizedBox(height: 40.0,),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10.0,
+                      ),
+                    ),
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+
+                  SizedBox(height: 40.0,),
+                  RaisedButton(onPressed: (){
+                    print("Something to tesr");
+                  },
+                  color: Colors.yellow,
+                  textColor: Colors.white,
+                  child: Container(
+                    height: 50.0,
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold"),
+                      ),
+                    ),
+                  ),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(24.0),
+                    ),
+                  ),
+                ],
+              ),
+              ),
+
+              FlatButton(onPressed: (){},
+                  child: Text(
+                    "Create New Account!",
+                  ))
+
+            ],
           ),
-
-          SizedBox(height: 15.0,),
-
-          Text(
-            "Login as a RIder",
-
-          )
-        ],
+        ),
       ),
     );
   }
