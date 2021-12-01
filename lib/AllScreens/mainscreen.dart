037c5d98ab15d65ab4017bdf7b71a7161dd0c703
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2021-12-01 15:43:58 +0000

    Adding Home and Work location panel - up to adding divider

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -43,6 +43,72 @@ class _MainScreenState extends State<MainScreen> {
             _controllerGoogleMap.complete(controller);
             newGoogleMapController = controller;
             },
+          ),
+
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 245.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                topRight: Radius.circular(18.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 16.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 6.0,),
+                    Text("Hi there", style: TextStyle(fontSize: 12.0, ),),
+                    Text("Home", style: TextStyle(fontSize: 12.0, fontFamily: "Brand-Bold"),),
+                    SizedBox(height: 20.0,),
+                    Container(
+                      height: 320,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 6.0,
+                              spreadRadius: 0.5,
+                              offset: Offset(0.7, 0.7),
+                            ),
+                          ],
+                        ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.search, color: Colors.yellowAccent,),
+                          SizedBox(height: 24.0,),
+
+                          Row(
+                            children: [
+                              Icon(Icons.home, color: Colors.grey,),
+                              SizedBox(height: 4.0,),
+                              Text("Home Address", style: TextStyle(color:  Colors.grey),),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
